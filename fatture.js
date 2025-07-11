7ac1a2e7e41e0cb78563d8d9b89a2366f227f24b
AluarSaaS <aluar.salvi@gmail.com>
2025-07-11 10:46:55 +0200

    Shoutout to Pietro Bosio aka mr Vallauri aka the best programmer aka mr. gayssss for helping me with fatture that is almost finished

--- fatture.js
+++ fatture.js
@@ -19,41 +19,42 @@ let anteprime = [
     titolo: "Lezione Privata - Matematica - Esponenziali",
     date: "15/06/2025",
     nome: "Mario Rossi",
-    // amount: "€25,00",
-    //imgSrc: "assets/arrow_right_60dp_000000_FILL0_wght400_GRAD0_opsz48.png"
+    panoramica: [
+      "Ripasso equazioni esponenziali",
+      "Ripasso disequazioni esponenziali",
+      "Ripasso proprietà delle esponenziali",
+      "Applicazione dell'equazione in grafico"
+    ],
+    orario: "15:30/17:00",
+    importo: "25.00"
   },
   {
     titolo: "Lezione Privata - Scienze - Duplicazione DNA",
     date: "14/06/2025",
     nome: "Mario Rossi",
-    // amount: "€25,00",
-    //imgSrc: "assets/arrow_right_60dp_000000_FILL0_wght400_GRAD0_opsz48.png"
   },
   {
     titolo: "Elaborazione piano nutrizionale",
     date: "14/06/2025",
     nome: "Marcello Rossi"
-    // amount: "€25,00",
-    //imgSrc: "assets/arrow_right_60dp_000000_FILL0_wght400_GRAD0_opsz48.png"
   },
    {
     titolo: "Lezione pubblica - Scienze - 3^ B Scientifico",
     date: "13/06/2025",
     nome: "Liceo Vallauri"
-    // amount: "€25,00",
-    //imgSrc: "assets/arrow_right_60dp_000000_FILL0_wght400_GRAD0_opsz48.png"
+
   },
   {
     titolo: "Lezione pubblica - Scienze - 3^ B Scientifico",
     date: "13/06/2025",
     nome: "Liceo Vallauri"
-    // amount: "€25,00",
-    //imgSrc: "assets/arrow_right_60dp_000000_FILL0_wght400_GRAD0_opsz48.png"
+    
   }
 ];
 
 anteprime = anteprime.map(anteprima => {
   let anteprimaSpec = document.createElement("div");
+  anteprimaSpec.stocazzo = anteprima;
   anteprimaSpec.classList.add("anteprimaSpec");
   // Imposta display grid e proprietà richieste
   anteprimaSpec.style.display = "grid";
@@ -110,10 +111,42 @@ anteprime = anteprime.map(anteprima => {
   return anteprimaSpec;
 });
 
+function mostraSpecificaFattura(fattura) {
+  const specificaFattura = document.querySelector('.specificaFattura');
+  console.log(fattura)
+  specificaFattura.innerHTML = `
+    <span class="dot"></span>
+    <div class="DivTitolo">
+      <span class="TitoloFattura">${fattura.titolo}</span>
+      <span class="SottoTitolo">${fattura.nome}</span>
+    </div>
+    <img class="IconaPanoramica" src="assets/overview_60dp_A764BD_FILL0_wght400_GRAD0_opsz48.png"/>
+    <div class="Panoramica">
+      <ul>
+        ${fattura.panoramica?.map(punto => `<li>${punto}</li>`)?.join('') ?? "Dettagli non disponibili"}
+      </ul>
+    </div>
+    <img class="IconaPanoramica" src="assets/calendar_clock_60dp_A764BD_FILL0_wght400_GRAD0_opsz48.png">
+    <table id="customers">
+      <tr>
+        <td class="pre">${fattura.date ?? "Data non disponibile"}<br>${fattura.orario  ?? "Orario non disponibile"}</td>
+        <td class="iconaMoney">
+          <img class="IconaPanoramica" src="assets/euro_symbol_60dp_A764BD_FILL0_wght400_GRAD0_opsz48.png"/>
+        </td>
+        <td>
+          <span class="Money">${fattura.importo ?? "Importo non disponibile"}</span>
+        </td>
+      </tr>
+    </table>
+  `;
+}
+
 anteprime.forEach((anteprima, index) => {
   anteprima.addEventListener("click", () => {
   anteprime.forEach((a) => {
     a.classList.toggle("active", a === anteprima);
   });
+   mostraSpecificaFattura(anteprima.stocazzo);
   });
 });
+
